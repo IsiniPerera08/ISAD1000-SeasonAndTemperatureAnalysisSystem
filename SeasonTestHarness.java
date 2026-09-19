51e32dfe00ed04b6cc722b53e31ae9d45ea13fe9
Isini Perera <isiniperera08@gmail.com>
2026-09-19 12:33:04 +0530

    Remove header comments from SeasonTestHarness.java
    Removed header comments from SeasonTestHarness.java.

--- SeasonTestHarness.java
+++ SeasonTestHarness.java
@@ -1,9 +1,3 @@
-/*
-Name: Isini Ayansa Perera
-Student ID: 23601321
-Purpose: test harness file used to implement and execute test cases related to the SeasonFinder methods
-*/
-
 public class SeasonTestHarness {
     static SeasonFinder seasonFind= new SeasonFinder();
     static int passed= 0;
